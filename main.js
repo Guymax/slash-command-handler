4cac168184836303a93bc054f0ea9dd90158dfc2
Guymax <guymaxyt@gmail.com>
2021-05-13 23:47:46 +0200

    removed useless intents

--- main.js
+++ main.js
@@ -2,27 +2,12 @@ const chalk = require("chalk");
 const Discord = require("discord.js");
 const { loadEvents } = require("./loader");
 
-const client = new Discord.Client({
-  partials: ["MESSAGE", "CHANNEL", "REACTION"],
-  ws: {
-    intents: [
-      "GUILDS",
-      "GUILD_MESSAGES",
-      "GUILD_PRESENCES",
-      "GUILD_BANS",
-      "GUILD_MEMBERS",
-      "GUILD_VOICE_STATES",
-      "GUILD_MESSAGE_REACTIONS",
-    ],
-  },
-});
+const client = new Discord.Client();
 
 client.config = require("./config");
-["guildSpecificCommands", "globalSlashCommands"].forEach(
-  (x) => (client[x] = new Discord.Collection())
-);
+["guildSpecificCommands", "globalSlashCommands"].forEach((x) => (client[x] = new Discord.Collection()));
 
 loadEvents(client);
 require("./functions")(client);
 
-client.login(client.config.TOKEN).then(console.log(chalk.yellow("Client Connecté")));
+client.login(client.config.TOKEN);
