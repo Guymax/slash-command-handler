8595737fd4c4d55406422c4b9342af9a5aef28ad
Guymax <guymaxyt@gmail.com>
2021-05-13 23:47:14 +0200

    added translation + prettier

--- events/ready.js
+++ events/ready.js
@@ -1,24 +1,20 @@
 const { loadCommands, resetCommands } = require("../loader");
 const chalk = require("chalk");
-const axios = require("axios");
 
 module.exports = (client) => {
-  console.log(`connecté en tant que ${client.user.username}`);
+  console.log(`Connected as ${chalk.cyanBright(client.user.username)}`);
   resetCommands(client).then(() => {
     loadCommands(client);
   });
 
   client.ws.on("INTERACTION_CREATE", async (interaction) => {
     const commandName = interaction.data.name.toLowerCase();
-    const args = interaction.data.options;
 
     await client.sendResponse(interaction, 5);
 
-    let command = client.guildSpecificCommands
-      .get(interaction.guild_id)
-      .get(commandName);
+    let command = client.guildSpecificCommands.get(interaction.guild_id).get(commandName);
     if (!command) {
-      // si jamais la commande n'est pas dans celles spécifique au serveur
+      // if this interaction isn't a guild specific command
 
       command = client.globalSlashCommands.get(commandName);
       if (!command) {
